1749598b7c0999bd6806a0743decf66a5801ecc2
Justin Bloomfield <jbloomfield@rkblack.com>
2022-12-22 10:37:02 -0600

    updated get-trmmagents

--- Public/Get-TrmmAgents.ps1
+++ Public/Get-TrmmAgents.ps1
@@ -1,36 +1,10 @@
-<#
-.SYNOPSIS
-    Gets a list of agents from Tactical RMM
-.DESCRIPTION
-    The Get-TrmmAgent cmdlet gets a list of agents in the Tactical RMM database.
-.PARAMETER Clients
-    Comma separated list of clients.  Pipeline input supported.
-.PARAMETER AgentNames
-    Comma separated list of agent names.  Pipeline input supported.
-.PARAMETER AgentType
-    Filter agents by type.  Valid values are 'Workstation' and 'Server'.
-.EXAMPLE
-    C:\PS> Get-TrmmAgents -Clients 'ACME Inc','Contoso','Some Other Client Name'
-.NOTES
-    Author: Justin Bloomfield
-    Date:   December 22, 2020
-#>
-
 function Get-TrmmAgents {
     [CmdletBinding()]
     param (
-        [Parameter(ValueFromPipeline = $true, AllowEmptyCollection = $true, PositionalBinding = $false)]
-        [string[]]
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullOrEmpty()]
-        [Alias('Client')]
+        [Parameter(ValueFromPipeline = $true)]
         [string[]]
         $Clients,
-        [Parameter(ValueFromPipeline = $true, AllowEmptyCollection = $true, PositionalBinding = $false)]
-        [string[]]
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullOrEmpty()]
-        [Alias('AgentName')]
+        [Parameter(ValueFromPipeline = $true)]
         [string[]]
         $AgentNames,
         [ValidateSet('Workstation', 'Server')]
@@ -40,33 +14,34 @@ function Get-TrmmAgents {
 
     $agents = @()
 
-    if ($AgentNames -and $Clients) {
-        Write-Error 'The AgentNames and Clients parameters cannot be used together'
-        return
-    }
-
     if ($AgentNames) {
-        foreach ($agentName in $AgentNames) {
-            $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.hostname -eq $agentName }
+        foreach ($agentName in $AgentNames) {            
             if ($AgentType) {
-                $filteredAgents = $filteredAgents | Where-Object { $_.monitoring_type -eq $AgentType }
+                $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.hostname -eq $agentName -and $_.monitoring_type -eq $AgentType }
+            }
+            else {
+                $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.hostname -eq $agentName }
             }
             $agents += $filteredAgents
         }
     }
     elseif ($Clients) {
-        foreach ($client in $Clients) {
-            $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.client.name -eq $client }
+        foreach ($client in $Clients) {     
             if ($AgentType) {
-                $filteredAgents = $filteredAgents | Where-Object { $_.monitoring_type -eq $AgentType }
+                $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.client_name -eq $client -and $_.monitoring_type -eq $AgentType }
+            }
+            else {
+                $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.client_name -eq $client }
             }
             $agents += $filteredAgents
         }
     }
-    else {
-        $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/')
+    else {  
         if ($AgentType) {
-            $filteredAgents = $filteredAgents | Where-Object { $_.monitoring_type -eq $AgentType }
+            $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/') | Where-Object { $_.monitoring_type -eq $AgentType }
+        }
+        else {
+            $filteredAgents = (Invoke-TrmmRequest -Method 'Get' -Resource '/agents/')
         }
         $agents += $filteredAgents
     }
